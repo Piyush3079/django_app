a4748a60ad5c7b2e160f03870f1060ce05df5abf
Piyush <piyushvijay.1997@gmail.com>
2017-03-17 06:08:45 +0530

    shortcut for 404 erroe

--- project_v1/views.py
+++ project_v1/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from .models import Question
-from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -11,10 +11,7 @@ def index(request):
 	return render(request,'app/index.html', data)
 
 def detail(request, question_id):
-	try:
-		question = Question.objects.get(pk = question_id)
-	except Question.DoesNotExist:
-		raise Http404("the Question you are looking for does not exist")
+	question = get_object_or_404(Question, pk=question_id)
 	data = {'question' : question}
 	return render(request, "app/detail.html", data)
 
