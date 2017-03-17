e3e977655165e60292bc4d007d994409646a0cc4
Piyush <piyushvijay.1997@gmail.com>
2017-03-17 06:05:12 +0530

    http404 error show

--- project_v1/views.py
+++ project_v1/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.template import loader
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Question
+from django.http import Http404
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('app/index.html')
 	data = {'latest_question_list': latest_question_list,}
-	return HttpResponse(template.render(data, request))
+	return render(request,'app/index.html', data)
 
 def detail(request, question_id):
-	return HttpResponse("you are looking at question %s." %question_id)
+	try:
+		question = Question.objects.get(pk = question_id)
+	except Question.DoesNotExist:
+		raise Http404("the Question you are looking for does not exist")
+	data = {'question' : question}
+	return render(request, "app/detail.html", data)
 
 def result(request, question_id):
-	return HttpResponse("you are looking at the result of question %s." %question_id)
+	return render(request, "you are looking at the result of question %s." %question_id)
 
 def vote(request, question_id):
-	return HttpResponse("you are voting on question %s." %question_id)
+	return render(request, "you are voting on question %s." %question_id)
