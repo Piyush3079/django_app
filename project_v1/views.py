18276048ef366c7e701d6384ddd52135832b13cb
Piyush <piyushvijay.1997@gmail.com>
2017-03-23 06:31:22 +0530

    form added

--- project_v1/views.py
+++ project_v1/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
-from .models import Question
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render, get_object_or_404
+from .models import Choice, Question
+from django.conf.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -19,4 +20,15 @@ def result(request, question_id):
 	return render(request, "you are looking at the result of question %s." %question_id)
 
 def vote(request, question_id):
-	return render(request, "you are voting on question %s." %question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except(KeyError, Choice.DoesNotExist):
+		return render(request, 'project_v1/detail.html',{
+			'question': question,
+			'error_message': "You didn't select a choice",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+	return HttpResponseRedirect(reverse('project_v1:results', args=(question.id,)))
